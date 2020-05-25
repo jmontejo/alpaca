d5c09c358fa2b0541237c641477256a9757ff7a6
Riccardo Poggi <riccardo.poggi@cern.ch>
2020-05-25 16:58:11 +0200

    refactored logging to be pythonic

--- alpaca/batch.py
+++ alpaca/batch.py
@@ -1,8 +1,11 @@
+import logging
+
 import pandas as pd
 import numpy as np
 import torch
 
-from alpaca.logging import log
+log = logging.getLogger(__name__)
+
 
 class BatchManager:
 
---
+++ alpaca/log.py
@@ -0,0 +1,24 @@
+import logging
+
+
+def setup_logger(file_path=None):
+    root_logger = logging.getLogger()
+
+
+    # stream handler
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    cf = logging.Formatter('[%(levelname)s] %(message)s')
+    ch.setFormatter(cf)
+    root_logger.addHandler(ch)
+
+    # file handler
+    if file_path is not None:
+        fh = logging.FileHandler(str(file_path))
+        fh.setLevel(logging.DEBUG)
+        ff = logging.Formatter('[%(asctime)s] %(levelname)s :: %(message)s')
+        fh.setFormatter(ff)
+        root_logger.addHandler(fh)
+
+
+    root_logger.setLevel(logging.DEBUG)
