28c886998befda0157549a50ad0733714bd3bbd2
Riccardo Poggi <riccardo.poggi@cern.ch>
2020-05-27 12:39:53 +0200

    doc and changed zero_jets wrt jets_per_event

--- alpaca/batch.py
+++ alpaca/batch.py
@@ -12,60 +12,110 @@ log = logging.getLogger(__name__)
 
 class BatchManager:
 
-    def __init__(self, sig_path, shuffle_jets=False, shuffle_events=False,
-                 zero_jets=0, jets_per_event=10):
-        self._labeledjets = self._get_jets(sig_path, shuffle_jets,
-                                          shuffle_events, zero_jets,
-                                          jets_per_event)
-        log.info('Nr. of signal events: %s', len(self._labeledjets))
+    def __init__(self, input_path, shuffle_jets=False, shuffle_events=False,
+                 jets_per_event=10, zero_jets=0):
+        """Refer to the documentation of the private method `_get_jets`."""
+        self._labeledjets = self._get_jets(
+            input_path=input_path,
+            shuffle_jets=shuffle_jets,
+            shuffle_events=shuffle_events,
+            jets_per_event=jets_per_event,
+            zero_jets=zero_jets
+        )
 
     @staticmethod
-    def _get_jets(file_path, shuffle_jets=False, shuffle_events=False,
-                 zero_jets=0, jets_per_event=10):
+    def _get_jets(input_path, shuffle_jets=False, shuffle_events=False,
+                 jets_per_event=10, zero_jets=0):
         """Function that reads an input file and returns a numpy array properly
         formatted, ready to be converted to pytorch tensors.
 
-        The returned object looks like this:
+        Args:
+            file_path: path to the input file
 
-        [[[event 1 jet 1 t, event 1 jet 1 x, event 1 jet 1 y, event 1 jet 1 z, event 1 jet 1 partonindex]
-          [event 1 jet 2 t, event 1 jet 2 x, event 1 jet 2 y, event 1 jet 2 z, event 1 jet 2 partonindex]
-            ...
-          [event 1 jet 10 t, event 1 jet 10 x, event 1 jet 10 y, event 1 jet 10 z, event 1 jet 10 partonindex]]
-          [[event 2 jet 1 t, event 2 jet 1 x, event 2 jet 1 y, event 1 jet 1 z, event 2 jet 1 partonindex]
+            shuffule_jets: if set to `True` for each event the jets are
+                shuffled, so that if they were pT ordered they will not be
+                anymore
+
+            shuffle_events: if set to `True` shuffles the evets order
+
+            jets_per_event: how many jets per event to have in the returned
+                object. This corresponds to the size of the inner array.
+
+            zero_jets: how many of the `jets_per_event` jets to set to zero. If
+                you don't shuffle the jets (i.e. `shuffle_jets` set to False)
+                the jets will be zero-padded at the end.
+
+        Return:
+            jet_stack: numpy array formatted as explained below
+
+
+        The returned object looks like this (in the case jets_per_event=5, zero_jets=0):
+
+        [[[event 1 jet 0 t, event 1 jet 0 x, event 1 jet 0 y, event 1 jet 0 z, event 1 jet 0 partonindex]
+          [event 1 jet 1 t, event 1 jet 1 x, event 1 jet 1 y, event 1 jet 1 z, event 1 jet 1 partonindex]
+          ...
+          [event 1 jet 5 t, event 1 jet 5 x, event 1 jet 5 y, event 1 jet 5 z, event 1 jet 5 partonindex]]
+         [[event 2 jet 1 t, event 2 jet 1 x, event 2 jet 1 y, event 1 jet 1 z, event 2 jet 1 partonindex]
           [event 2 jet 2 t, event 2 jet 2 x, event 2 jet 2 y, event 1 jet 2 z, event 2 jet 2 partonindex]
-           ...
+          ...
 
         basically they are separated by events. For each event they are
-        separated by jet: jet 1, jet 2, etc. And for each jet the five elements
+        separated by jet: jet 0, jet 1, etc. And for each jet the five elements
         are the four coordinates of the TLorentz vector: t, x, y, z; plus a
-        truth-based parton label (0=not top jet, 1-3 = b,Wa,Wb from top, 4-6
-        ditto from antitop).
-        The jets are zero-padded up to the 10th and pT-ordered.
+        truth-based parton label that is an integer number betwwen 0 and 6
+        (0 = not top jet, 1-3 = b,Wa,Wb from top, 4-6 ditto from antitop).
         """
 
-        df = pd.read_hdf(file_path, "df")
-        # These next lines can be used to filter out some events, e.g. to
-        # limit the training & evaluation to N>6 leading jets
-        leadingNcontaintop = df[[("partonindex", i) for i in range(10 - zero_jets, 10)]].sum(axis=1) < 1
-        leadingNarenonzero = df[[("jet_e", i) for i in range(10 - zero_jets, 10)]].sum(axis=1) < 1
-        df = df[leadingNcontaintop & leadingNarenonzero]
-        #
+        df = pd.read_hdf(input_path, "df")
+        # Get the number of jets per event in the input file by inspecting the
+        # second level index of one of the columns
+        tot_jets_per_event = len(df['partonindex'].columns.get_level_values('subentry'))
+
+        if (jets_per_event - zero_jets) > tot_jets_per_event:
+            log.warning(
+                'You are asking for %s jets, but only %s are available',
+                jets_per_event - zero_jets,
+                tot_jets_per_event
+            )
+
+        # Select only those events in which
+        # - a top jet is not going to be cut, by checking that among the
+        #    remaining jets after the Nth there aren't any (note the minus sign)
+        # - the leading jets are all existent
+        leadingNincludealltop = - df[[("partonindex", i) for i in range(jets_per_event - zero_jets, tot_jets_per_event)]].any(axis=1)
+        leadingNarenonzero = df[[("jet_e", i) for i in range(jets_per_event - zero_jets)]].all(axis=1)
+        df = df[leadingNincludealltop & leadingNarenonzero]
+
         if shuffle_events:
             df.reindex(np.random.permutation(df.index))
-        nevents = len(df)
+
         # The input rows have all jet px, all jet py, ... all jet partonindex
         # So segment and swap axes to group by jet
-        jet_stack = np.swapaxes(df.values.reshape(nevents, 5, 10), 1, 2)
+        jet_stack = np.swapaxes(df.values.reshape(len(df), 5, 10), 1, 2)
         jet_stack = jet_stack[:, :jets_per_event, :]
         if shuffle_jets:
             # shuffle only does the outermost level
             # iterate through rows to shuffle each event individually
             for row in jet_stack:
                 np.random.shuffle(row)
+
         return jet_stack
 
     def get_torch_batch(self, N, nlabels, start_index=0):
-        """Function that returns pytorch tensors with a batch of events."""
+        """Function that returns pytorch tensors with a batch of events,
+        specifically events in the range [start_index:start_index+N] from the
+        input file.
+
+        Args:
+            N: the number of events to return
+            nlabels: ??
+            start_index: starting index of the file
+
+        Return:
+            (X, Y): X is in the input to the pytorch model and Y is the truth
+                    information associated to X.
+
+        """
         stop_index = start_index + N
         if stop_index > len(self._labeledjets):
             log.warning('The stop index is greater than the size of the array')
@@ -103,3 +153,6 @@ class BatchManager:
         X = torch.as_tensor(jets_clean, dtype=torch.float)
         Y = torch.as_tensor(jetlabels_clean, dtype=torch.float)
         return X, Y
+
+    def get_tot_events(self):
+        return len(self._labeledjets)
