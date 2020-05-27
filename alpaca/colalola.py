e216954a7eca7e68d1ac6e84ac27a1896302052d
Riccardo Poggi <riccardo.poggi@cern.ch>
2020-05-27 14:47:45 +0200

    removed feedforward

--- alpaca/colalola.py
+++ alpaca/colalola.py
@@ -63,23 +63,6 @@ class LoLa(torch.nn.Module):
         return outputs, masses.flatten().detach().numpy()
 
 
-class FeedForwardHead(torch.nn.Module):
-
-    def __init__(self, sizes=None):
-        super(FeedForwardHead, self).__init__()
-        self.linears = torch.nn.ModuleList([torch.nn.Linear(sizes[i-1], sizes[i]) for i in range(1, len(sizes))])
-        activations = [torch.nn.ReLU() for i in range(len(sizes) - 2)]
-        activations.append(torch.nn.Sigmoid())
-        self.activations = torch.nn.ModuleList(activations)
-
-    def forward(self, x):
-        output = x
-        for i in range(len(self.linears)):
-            output = self.linears[i](output)
-            output = self.activations[i](output)
-        return output
-
-
 class CoLaLoLa(torch.nn.Module):
 
     def __init__(self, nobjects, ncombos, noutputs, fflayers=[200]):
