20102146f0b5d81ebeb2b8855544d6d981cd4b6d
Javier Montejo Berlingen <jmontejo@cern.ch>
2020-08-18 11:35:22 +0200

    Can run my signal :)

--- alpaca/batch.py
+++ alpaca/batch.py
@@ -43,8 +43,9 @@ class BatchManager:
         # Account for charge ambiguity by identifying whether the
         # jets match the leading jet or not
         topmatch = np.array([r > 3 for r in labels])
+        dummymatch = np.array([r > 4 for r in labels])
         isbjet = np.array([np.equal(r, 1) | np.equal(r, 4) for r in labels])
-        jetlabels = np.concatenate([jetfromttbar, topmatch, isbjet], 1)
+        jetlabels = np.concatenate([jetfromttbar, topmatch, dummymatch, isbjet], 1)
         # Substitute this line for the preceding if only doing the 6 top jets
         # Not currently configurable by command line because it's a bit more
         # complicated overall + less often changed
@@ -158,3 +159,98 @@ class BatchManager:
         """
         # self._jets and self._jetslabels should have the same length
         return len(self._jets)
+
+class BatchManager2HDM (BatchManager): #need to include lepton 4-vectors, MET
+
+    def __init__(self, input_path, shuffle_jets=False, shuffle_events=False,
+                 jets_per_event=10, zero_jets=0):
+
+        if "truthmatched.h5" in str(input_path):
+            return super().__init__(input_path, shuffle_jets, shuffle_events, jets_per_event, zero_jets)
+
+        """Refer to the documentation of the private method `_get_jets`."""
+        labeledjets, rest = self._get_jets_and_leps(
+            input_path=input_path,
+            shuffle_jets=shuffle_jets,
+            shuffle_events=shuffle_events,
+            jets_per_event=jets_per_event,
+            zero_jets=zero_jets
+        )
+
+        # The rest of this method is about parsing the partonindex labels to
+        # to derive the truth labels that can be used by the NN.
+        # At the end there is also additional sanitisation of the input files
+        # which removes some events.
+        jets = labeledjets[:, :, :4] #drop parton index, keep 4-vector
+        labels = np.array(labeledjets[:, :, -1:].squeeze(), dtype=int) #only parton index
+        lep0 = rest[:,:4]
+        lep1 = rest[:,4:8]
+        met  = rest[:,8:]
+        met  = np.concatenate([met,np.zeros([len(met),2])],axis=1)
+
+        def myjetlabels(labels):
+            myisr = [j==0 for j in labels]
+            myfromlep0 = [j==1 for j in labels]
+            myfromlep1 = [j==2 for j in labels]
+            myfromhad0 = [j==3 for j in labels]
+            return np.concatenate([myisr,myfromlep0,myfromlep1,myfromhad0],axis=1) #FIXME
+
+        lep_met = np.stack([lep0,lep1,met],axis=1)
+        lep_met_jets = np.concatenate([lep_met,jets],axis=1)
+
+        self._jets = lep_met_jets
+        self._jetlabels = myjetlabels(labels) #len njets*4
+
+
+    @staticmethod
+    def _get_jets_and_leps(input_path, shuffle_jets=False, shuffle_events=False,
+                           jets_per_event=10, zero_jets=0):
+        """See _get_jets"""
+
+        df = pd.read_hdf(input_path, "df")
+        # Get the number of jets per event in the input file by inspecting the
+        # second level index of one of the columns
+
+        tot_jets_per_event = len(df['jet_e'].columns.get_level_values('subentry'))
+
+        if (jets_per_event - zero_jets) > tot_jets_per_event:
+            log.warning(
+                'You are asking for %s jets, but only %s are available',
+                jets_per_event - zero_jets,
+                tot_jets_per_event
+            )
+
+
+        #KEEP ALL EVENTS
+        #leadingNincludealltop = - df[[("partonindex", i) for i in range(jets_per_event - zero_jets, tot_jets_per_event)]].any(axis=1)
+        #leadingNarenonzero = df[[("jet_e", i) for i in range(jets_per_event - zero_jets)]].all(axis=1) #complicated cut on at least N jets
+        #df = df[leadingNincludealltop & leadingNarenonzero]
+
+        if shuffle_events:
+            df.reindex(np.random.permutation(df.index))
+
+        # The input rows have all jet px, all jet py, ... all jet partonindex
+        # So segment and swap axes to group by jet
+        jet_vars = ['jet_pt','jet_eta','jet_phi','jet_e','jet_partonindex']
+        rest_vars = ['lep0_pt','lep0_eta', 'lep0_phi','lep0_e','lep1_pt','lep1_eta', 'lep1_phi','lep1_e','met','met_phi']
+        maxjets = 8
+        #assert len(jet_vars)*2==len(rest_vars) #I'm abusing two leading slots for lep+met
+        jet_df = df[jet_vars]
+        rest_df = df[rest_vars].droplevel(1,axis=1)
+        rest_df = rest_df.loc[:,~rest_df.columns.duplicated()]
+
+        print(jet_df.head())
+        print(rest_df.head())
+
+        jet_stack = np.swapaxes(jet_df.values.reshape(len(df), len(jet_vars), maxjets), 1, 2)
+        jet_stack = jet_stack[:, :jets_per_event, :]
+        #rest_stack = np.swapaxes(rest_df.values.reshape(len(df), len(jet_vars), 2), 1, 2)
+        rest_stack = rest_df.values
+
+        if shuffle_jets:
+            # shuffle only does the outermost level
+            # iterate through rows to shuffle each event individually
+            for row in jet_stack:
+                np.random.shuffle(row)
+
+        return jet_stack, rest_stack
