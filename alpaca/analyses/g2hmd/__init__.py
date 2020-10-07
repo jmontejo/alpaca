4b53375b7f148aafd536c130d8970d921d6299a8
Javier Montejo Berlingen <jmontejo@cern.ch>
2020-10-07 16:26:38 +0200

    All-had ttbar mode

--- alpaca/analyses/g2hmd/__init__.py
+++ alpaca/analyses/g2hmd/__init__.py
@@ -23,9 +23,7 @@ def register_cli(subparser):
 
     # Create your own sub-command and add arguments
     parser = subparser.add_parser(analysis_name,
-                                   help='Hello world sub-command.')
-    parser.add_argument('--example', action='store_true',
-                        help='example argument')
+                                   help='Flavourful 2HDM sub-command.')
     parser.add_argument('--input-files', '-i', required=True, type=Path,
                         action='append',
                         help='path to the file with the input events')
