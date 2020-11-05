aad91c4b54a834f6118f61f3cd81e3400c04409e
Chiara Rizzi <chiara.rizzi@cern.ch>
2020-11-05 11:06:24 +0100

    add mass from true combination

--- scripts/plotm.py
+++ scripts/plotm.py
@@ -25,7 +25,7 @@ t=f.Get(tname)
 nentries=t.GetEntries()
 for ientry in range(nentries):
     t.GetEntry(ientry)
-    jets = []
+    jets_all = []
     jet_vars = [
         (t.jet_px_0, t.jet_py_0, t.jet_pz_0, t.jet_e_0),
         (t.jet_px_1, t.jet_py_1, t.jet_pz_1, t.jet_e_1),
@@ -37,11 +37,11 @@ for ientry in range(nentries):
         ]
     njets= len(jet_vars)
     for i in range(njets):
-        jets.append(ROOT.TLorentzVector())
+        jets_all.append(ROOT.TLorentzVector())
     for i,v in enumerate(jet_vars):
-        jets[i].SetPxPyPzE(v[0]/1000., v[1]/1000., v[2]/1000., v[3]/1000.) # convert to GeV
+        jets_all[i].SetPxPyPzE(v[0]/1000., v[1]/1000., v[2]/1000., v[3]/1000.) # convert to GeV
     # print('mass of each jet')
-    # for j in jets:
+    # for j in jets_all:
     #    print(j.M())
     from_top = [t.from_top_0, t.from_top_1, t.from_top_2, t.from_top_3, t.from_top_4, t.from_top_5, t.from_top_6]
     # print(from_top)
@@ -50,7 +50,7 @@ for ientry in range(nentries):
     pred_from_top = [1 for i in range(njets)]
     pred_from_top[idx_isr]=0
     # print(pred_from_top)
-    jets = [j for i,j in enumerate(jets) if pred_from_top[i]>0] # remove jet from ISR
+    jets = [j for i,j in enumerate(jets_all) if pred_from_top[i]>0] # remove jet from ISR
     # print('mass of each jet')
     # for j in jets:
         # print(j.M())    
@@ -82,6 +82,30 @@ for ientry in range(nentries):
     t1 = t1_list[0]+t1_list[1]+t1_list[2]
     t2_list = [t for i,t in enumerate(jets) if pred_same_as_lead[i]<1]
     t2 = t2_list[0]+t2_list[1]+t2_list[2]
+
+    # true info
+    true_from_top = [t.from_top_0_true, t.from_top_1_true, t.from_top_2_true, t.from_top_3_true, t.from_top_4_true, t.from_top_5_true, t.from_top_6_true]
+    jets_true = [j for i,j in enumerate(jets_all) if true_from_top[i]>0] # remove jet from ISR
+    true_is_b = [t.is_b_0_true, t.is_b_1_true, t.is_b_2_true, t.is_b_3_true, t.is_b_4_true, t.is_b_5_true]
+    true_same_as_lead = [t.same_as_lead_0_true, t.same_as_lead_1_true, t.same_as_lead_2_true, t.same_as_lead_3_true, t.same_as_lead_4_true]
+    true_same_as_lead.insert(0,1)
+    t1_list_true = [t for i,t in enumerate(jets_true) if true_same_as_lead[i]>0]
+    t1_true = t1_list_true[0]+t1_list_true[1]+t1_list_true[2]
+    t2_list_true = [t for i,t in enumerate(jets_true) if true_same_as_lead[i]<1]
+    t2_true = t2_list_true[0]+t2_list_true[1]+t2_list_true[2]
+
+    if t2.Pt() > t1.Pt(): # call t1 the top with leading pt
+        t_appo = t1
+        t1 = t2 
+        t2 = t_appo
+    if t2_true.Pt() > t1_true.Pt(): # call t1 the top with leading pt
+        t_appo_true = t1_true
+        t1_true = t2_true
+        t2_true = t_appo_true
+
     if ientry%1000==0:
         print(ientry)
-        print('M1:',t1.M(), ' M2:',t2.M())
+        print('M1 reco:',t1.M(),      ' M2 reco:',t2.M())
+        print('M1 true:',t1_true.M(), ' M2 true:',t2_true.M())
+
+
