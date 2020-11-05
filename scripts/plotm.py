d5341b1da51e014abf9c5db4ead6d9cc569c4fe5
Chiara Rizzi <chiara.rizzi@cern.ch>
2020-11-05 15:57:11 +0100

    continute script

--- scripts/plotm.py
+++ scripts/plotm.py
@@ -3,13 +3,38 @@ import numpy as np
 from root_pandas import to_root
 import ROOT
 from array import array
+import uproot
+import progressbar
 
-filename='/Users/crizzi/lavoro/SUSY/RPV/alpaca_mio/mytest.csv'
-df=pd.read_csv(filename)
-#print(df.head())
+filename_chi2 = '/Users/crizzi/lavoro/SUSY/RPV/alpaca_mio/alpaca_example_files/AT/test_in.410471.root'
+tname_chi2 = 'nominal'
+f_chi2 = uproot.open(filename_chi2)
+t_chi2 = f_chi2[tname_chi2]
+#print(t_chi2.show())
+df_chi2 = t_chi2.pandas.df(['eventNumber', 'reco_Chi2Fitted', 'reco_t1_m', 'reco_t2_m', 'jet_pt*'], flatten=False)
+df_chi2['njets'] = df_chi2['jet_pt'].apply(lambda x: len(x))
+df_chi2['njets_25'] = df_chi2['jet_pt'].apply(lambda x: len([j for j in x if j > 25000]))
+df_chi2['pt_jet_0'] = df_chi2['jet_pt'].apply(lambda x: x[0]/1000.)
+df_chi2['pt_jet_0_merge'] = round(df_chi2['pt_jet_0'],4)
+df_chi2 = df_chi2[['eventNumber', 'reco_Chi2Fitted', 'reco_t1_m', 'reco_t2_m', 'njets', 'njets_25','pt_jet_0','pt_jet_0_merge']]
+print(df_chi2.shape)
+print(df_chi2.head(15))
+
+filename_alpaca='/Users/crizzi/lavoro/SUSY/RPV/alpaca_mio/mytest.csv'
+df_alpaca=pd.read_csv(filename_alpaca)
+df_alpaca['pt_jet_0'] = np.sqrt(df_alpaca['jet_px_0']*df_alpaca['jet_px_0'] + df_alpaca['jet_py_0']*df_alpaca['jet_py_0'])/1000.
+df_alpaca['pt_jet_0_merge'] = round(df_alpaca['pt_jet_0'], 4)
+print(df_alpaca.shape)
+print(df_alpaca[['pt_jet_0','pt_jet_0_merge']].head())
+
+
+df = pd.merge(df_alpaca[['pt_jet_0_merge']], df_chi2, left_on='pt_jet_0_merge', right_on='pt_jet_0_merge', how='outer')
+print(df.shape)
+print(df.head())
 #print(df.columns)
 fname='df_alpaca.root'
 tname='tree_alpaca'
+
 to_root(df, fname, key=tname)
 # branches
 # 'jet_px_0', 'jet_py_0', 'jet_pz_0', 'jet_e_0' ... 'jet_e_6'
@@ -37,7 +62,7 @@ t_out.Branch("mt2_true",  mt2_true, "mt2_true/D")
 f=ROOT.TFile.Open(fname,'READ')
 t=f.Get(tname)
 nentries=t.GetEntries()
-for ientry in range(nentries):
+for ientry in progressbar.progressbar(range(nentries)):
     t.GetEntry(ientry)
     jets_all = []
     jet_vars = [
@@ -112,10 +137,10 @@ for ientry in range(nentries):
     mt2_true[0] = t2_true.M()
     t_out.Fill()
 
-    if ientry%1000==0:
-        print(ientry)
-        print('M1 reco:',t1.M(),      ' M2 reco:',t2.M())
-        print('M1 true:',t1_true.M(), ' M2 true:',t2_true.M())
+    #if ientry%1000==0:
+    #    print(ientry)
+    #    print('M1 reco:',t1.M(),      ' M2 reco:',t2.M())
+    #    print('M1 true:',t1_true.M(), ' M2 true:',t2_true.M())
 
 f_out.Write()
 f_out.Close()
