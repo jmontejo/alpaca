41809f8ef6a5acd8d5312fae4c7020ecad7421f1
Chiara Rizzi <chiara.rizzi@cern.ch>
2021-02-07 20:21:18 +0100

    updates

--- scripts/GluinoJetMatching.py
+++ scripts/GluinoJetMatching.py
@@ -3,64 +3,69 @@
 import uproot, uproot_methods, math
 import numpy as np
 import pandas as pd
+import sys
 
 pd.set_option('display.max_columns', None)
 
-folder = "/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/zoom_recordings/alpaca_tutorial/user.rpoggi.410471.PhPy8EG.DAOD_TOPQ1.e6337_e5984_s3126_r9364_r9315_p3629.TTDIFFXS36_R21_allhad_resolved.root/"
+folder = '/eos/user/c/crizzi/RPV/ntuples/FT_signal_020321_merged/mc16e/signal/'
 
 store_truth = True
 select_truth = False
-select_2bTag = True
-
-#h5_name='/eos/user/c/crizzi/RPV/alpaca/h5/mio/truthmatched_btagging_2b_train_36_2_3_4.h5'
-# Open root file
-#fname = [folder+"*_00002*.allhad_resolved.root", folder+"*_00003*.allhad_resolved.root", folder+"*_00004*.allhad_resolved.root"]
-#fname = [folder+"user.rpoggi.18378247.*root"]
-#fname = [folder+"user.rpoggi.18378247.*00030*root"]
 
 # h5_name='/eos/user/c/crizzi/RPV/alpaca/h5/mio/truthmatched_unfiltered_test_36_0_1.h5'
-h5_name='/eos/user/c/crizzi/RPV/alpaca/h5/mio/truthmatched_btagging_2b_test_36_0_1_unfiltered.h5'
-fname = [folder+"*_00000*.allhad_resolved.root", folder+"*_00001*.allhad_resolved.root"]
+h5_name='/eos/user/c/crizzi/RPV/alpaca/h5/mio/truthmatched_gluino_UDB_1200.h5'
+fname = [folder+'/504516.root']
 
 print("Working on file '{}'".format(fname))
 # Define parton labels and corresponding indices
 # 0 denotes a failure to match
-partons = [ '{}_from_{}'.format(child,parent) for parent in ['t','tbar'] for child in ['b','Wdecay1','Wdecay2']]
+partons = [ '{}_from_{}'.format(child,parent) for parent in ['g1','g2'] for child in ['q1','q2','q3']]
 partonindex = {parton:iparton for iparton,parton in enumerate([None]+partons)}
+
+print(partons)
 
 # Access trees as dataframe iterators reading multiple input files
 print("Making pandas")
-nom_iter = uproot.pandas.iterate(fname,"nominal",entrysteps=float("inf"),branches=["eventNumber","jet_*"])
-truth_iter = uproot.pandas.iterate(fname,"truth",entrysteps=float("inf"),branches=["MC_b_from_t*","MC_Wdecay*_from_t*"])
-# Set the truth df index so we can use "join"
-# Join function seems to preserve subentry, which merge does not
-truth_eventN_iter = uproot.pandas.iterate(fname,"truth",entrysteps=float("inf"),branches="eventNumber")
+nom_iter = uproot.pandas.iterate(fname,"trees_SRRPV_",entrysteps=float("inf"),branches=["eventNumber","jet_*"])
+truth_iter = uproot.pandas.iterate(fname,"trees_SRRPV_",entrysteps=float("inf"),branches=["eventNumber","truth_QuarkFromGluino_phi",'truth_QuarkFromGluino_eta'])#, 'truth_QuarkFromGluino_ParentBarcode'])
 
 df_list=[]
 ifile = 0
-for nom_df, truth_df, truth_eventN_df in zip(nom_iter,truth_iter,truth_eventN_iter):
+for nom_df,truth_df  in zip(nom_iter,truth_iter):
     print("Processing file {}".format(ifile))
     # Merge dataframes on "eventNumber" key
     # Merging truth into nominal retains only entries in nominal tree
     n_nom = nom_df.unstack(fill_value=0).shape[0]
-    truth_df.index = truth_eventN_df["eventNumber"]
-    truth_df['n_partons_truth'] = sum([truth_df["MC_"+p+"_m"]>0 for p in partons])
-    #    truth_df['n_partons'] = truth_df.apply(lambda row: sum([row["MC_"+p+"_m"]>0 for p in partons]) )
-
+    # print(nom_df.head())
+    print(truth_df.head())
+    col_orig = truth_df.columns.get_level_values(0) 
+    truth_df = truth_df.unstack(level=-1)
+    col_new = [c+'_'+str(n) for c in col_orig for n in range(6)]
+    #print(col_new)
+    #print(truth_df.columns)
+    #print(truth_df.columns.droplevel(0))
+    truth_df.columns = col_new
+    truth_df["eventNumber"] = truth_df["eventNumber_0"]
+    truth_df.index = truth_df["eventNumber"]
+    to_keep = [c for c in truth_df.columns if 'truth' in c]
+    truth_df = truth_df[to_keep]
+    col_new = [c+'_'+v for v in ['phi','eta'] for c in partons]
+    print(truth_df.head())
+    print(truth_df.columns)
+    print(col_new)
+    truth_df.columns = col_new
+    truth_df['n_partons_truth'] = sum([truth_df[p+"_phi"]*truth_df[p+"_phi"]>0 for p in partons])
 
     joined_df = nom_df.join(truth_df,on="eventNumber")
-    joined_df = joined_df[joined_df["eventNumber"].notna()]
-
-    # print(truth_df.head())
-    # print(nom_df.head())
 
     print("  Computing dR")
     # Compute delta eta and delta phi
     twopi = 2*math.pi # mod takes precedence over times, so precompute
     for parton in partons:
-        joined_df["jet_deta_{}".format(parton)] = joined_df["jet_eta"] - joined_df["MC_{}_eta".format(parton)]
-        joined_df["jet_dphi_{}".format(parton)] = (joined_df["jet_phi"] - joined_df["MC_{}_phi".format(parton)] + math.pi) % twopi - math.pi
+        joined_df["jet_deta_{}".format(parton)] = joined_df["jet_eta"] - joined_df["{}_eta".format(parton)]
+        joined_df["jet_dphi_{}".format(parton)] = (joined_df["jet_phi"] - joined_df["{}_phi".format(parton)] + math.pi) % twopi - math.pi
         joined_df["jet_dR_{}".format(parton)] = np.sqrt(joined_df["jet_deta_{}".format(parton)]**2 + joined_df["jet_dphi_{}".format(parton)]**2)    
+
 
     if store_truth:
         # Apply jet-parton matching
@@ -74,13 +79,15 @@ for nom_df, truth_df, truth_eventN_df in zip(nom_iter,truth_iter,truth_eventN_it
     joined_df["jet_px"] = joined_df["jet_pt"]*np.cos(joined_df["jet_phi"])
     joined_df["jet_py"] = joined_df["jet_pt"]*np.sin(joined_df["jet_phi"])
     joined_df["jet_pz"] = joined_df["jet_pt"]*np.sinh(joined_df["jet_eta"])
-    joined_df["jet_isDL177"] = joined_df["jet_DL1"]>1.45
+    # joined_df["jet_isDL177"] = joined_df["jet_DL1"]>1.45
         
     # Truncate the jet vector
     # maxjets = 10
     truncated_df = joined_df # joined_df[joined_df.index.get_level_values('subentry') < maxjets] # chiara: keep all jets
     truncated_df = truncated_df.unstack(fill_value=0)
     n_truncated = truncated_df.shape[0]
+
+    print(partonindex)
     
     if store_truth:
         def good_labels(df, ij=None): 
@@ -122,31 +129,29 @@ for nom_df, truth_df, truth_eventN_df in zip(nom_iter,truth_iter,truth_eventN_it
 
     truncated_df["event_number",0] = truncated_df["eventNumber",0]
     truncated_df["n_jets",0] = np.count_nonzero(truncated_df['jet_e']>0 , axis=1)
-    truncated_df["n_DL177",0] = np.count_nonzero(truncated_df['jet_isDL177']>0 , axis=1)
+    #truncated_df["n_DL177",0] = np.count_nonzero(truncated_df['jet_isDL177']>0 , axis=1)
         
     filtered_df = truncated_df
-    if select_2bTag:
-        filtered_df = filtered_df[ filtered_df["n_DL177",0]>1 ] 
     if select_truth:
         filtered_df = filtered_df[(filtered_df['n_matched',0]==6)&(filtered_df['n_partons',0]>5)]
 
     for ij in range(0,20):
-        for col in ["partonindex"]+["jet_"+comp for comp in["e","px","py","pz","isDL177"]]:
+        for col in ["partonindex"]+["jet_"+comp for comp in["e","px","py","pz"]]:
             if not (col,ij) in filtered_df.columns:
                 filtered_df[col,ij] = 0
 
     # Reshape the df to have one event per row,
     # filling each overflow entry with 0's
     # Extract just the cartesian coordinates and the parton index
-    jet_cartesian = ["jet_"+comp for comp in ["e","px","py","pz","isDL177"]]
+    jet_cartesian = ["jet_"+comp for comp in ["e","px","py","pz"]]
     # chiare: somehow some events become all nan
     filtered_df = filtered_df[filtered_df['eventNumber'].notna()]
     if store_truth:
         #filtered_df["partonindex"] = filtered_df["partonindex"].astype(int)            
         # unstacked_df = filtered_df[jet_cartesian+["partonindex","event_number","n_partons","n_matched","n_matched_6","good_match","n_jets","n_DL177"]+["good_match_"+str(ij)+"j" for ij in [6,7,8,9,10,11,12]]]
-        unstacked_df = filtered_df[jet_cartesian+["partonindex","event_number","good_match","n_jets","n_DL177"]]
+        unstacked_df = filtered_df[jet_cartesian+["partonindex","event_number","good_match","n_jets"]]
     else:
-        unstacked_df = filtered_df[jet_cartesian+["event_number","n_DL177","n_jets"]]
+        unstacked_df = filtered_df[jet_cartesian+["event_number","n_jets"]]
 
     unstacked_df.fillna(0,inplace=True)
     #print(unstacked_df[["n_jets","n_partons","n_matched","n_matched_6","good_match"]+["good_match_"+str(ij)+"j" for ij in [6,7,8,9,10,11,12]]].describe())
