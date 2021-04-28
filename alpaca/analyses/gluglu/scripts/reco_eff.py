b315c0af5fde84eb50b63801ef6261646d92b0a8
Chiara Rizzi <chiara.rizzi@cern.ch>
2021-04-28 10:51:00 +0200

    update script reco_eff.py for gluglu

--- alpaca/analyses/gluglu/scripts/reco_eff.py
+++ alpaca/analyses/gluglu/scripts/reco_eff.py
@@ -12,34 +12,46 @@ by_file = True
 files = [
     '/eos/user/c/crizzi/RPV/alpaca/results/alpaca_8j_hydra_3layers_UDS_1400_onlyBaseline/outputtree_test.root',
     '/eos/user/c/crizzi/RPV/alpaca/results/alpaca_8j_hydra_3layers_UDS_1400_isSig/outputtree_test.root',
-    '/eos/user/c/crizzi/RPV/alpaca/results/alpaca_8j_hydra_3layers_UDS_1400_isSig_isGluon/outputtree_test.root'
+    '/eos/user/c/crizzi/RPV/alpaca/results/alpaca_8j_hydra_3layers_UDS_1400_isSig_isGluon/outputtree_test.root',
+    '/eos/user/c/crizzi/RPV/alpaca/results/alpaca_8j_hydra_3layers_UDS_1400_partonindex/outputtree_test.root'
 ]
 
-def print_eff(tree, jet_sel,max_jet):
+def print_eff(tree, jet_sel,max_jet, has_mg1g2=True):
     for j in jet_sel:
         #print(j)
         total = float(tree.GetEntries("1 &&"+j))
         has_truth = float(tree.GetEntries("has_truth &&"+j))    
-        #chi2_good_mt1 = round(100*(tree.GetEntries("chi2_good_mt1==1 &&"+j))/has_truth,1)
-        #chi2_good_mt2 = round(100*(tree.GetEntries("chi2_good_mt2==1 &&"+j))/has_truth,1)
-        #chi2_good_both = round(100*(tree.GetEntries("chi2_good_mt2==1 && chi2_good_mt1==1 &&"+j))/has_truth,1)
-        n_alpaca_good_mt1 = tree.GetEntries("alpaca_good_mt1==1 &&"+j)
-        alpaca_good_mt1 = round(100*(n_alpaca_good_mt1)/has_truth,1)
-        n_alpaca_good_mt2 = tree.GetEntries("alpaca_good_mt2==1 &&"+j)
-        alpaca_good_mt2 = round(100*(n_alpaca_good_mt2)/has_truth,1)
-        n_alpaca_good_both = tree.GetEntries("alpaca_good_mt2==1 && alpaca_good_mt1==1 &&"+j)
+        #chi2_good_mg1 = round(100*(tree.GetEntries("chi2_good_mg1==1 &&"+j))/has_truth,1)
+        #chi2_good_mg2 = round(100*(tree.GetEntries("chi2_good_mg2==1 &&"+j))/has_truth,1)
+        #chi2_good_both = round(100*(tree.GetEntries("chi2_good_mg2==1 && chi2_good_mg1==1 &&"+j))/has_truth,1)
+        n_alpaca_good_mg1 = tree.GetEntries("alpaca_good_mg1==1 &&"+j)
+        alpaca_good_mg1 = round(100*(n_alpaca_good_mg1)/has_truth,1)
+        n_alpaca_good_mg2 = tree.GetEntries("alpaca_good_mg2==1 &&"+j)
+        alpaca_good_mg2 = round(100*(n_alpaca_good_mg2)/has_truth,1)
+        n_alpaca_good_both = tree.GetEntries("alpaca_good_mg2==1 && alpaca_good_mg1==1 &&"+j)
         alpaca_good_both = round(100*(n_alpaca_good_both)/has_truth,1)
+        n_alpaca_good_mg1g2 = tree.GetEntries("alpaca_good_mg1g2==1 &&"+j)
 
         has_truth_wrttot = round(100*(has_truth)/total,1)
-        alpaca_good_mt1_wrttot = round(100*(n_alpaca_good_mt1)/total,1)
-        alpaca_good_mt2_wrttot = round(100*(n_alpaca_good_mt2)/total,1)
+        alpaca_good_mg1_wrttot = round(100*(n_alpaca_good_mg1)/total,1)
+        alpaca_good_mg2_wrttot = round(100*(n_alpaca_good_mg2)/total,1)
         alpaca_good_both_wrttot = round(100*(n_alpaca_good_both)/total,1)
         
-        #print(j, ' & ', chi2_good_mt1, '\\% & ', chi2_good_mt2, '\\% & ', chi2_good_both, '\\% & ', alpaca_good_mt1, '\\% & ', alpaca_good_mt2, '\\% & ', alpaca_good_both,'\\% \\\\')
-        #print(max_jet,' & ',j.replace('njets>=5','Inclusive'), ' & ', alpaca_good_mt1, '\\% & ', alpaca_good_mt2, '\\% & ', alpaca_good_both,
-        print(max_jet,' & ', has_truth_wrttot , '\\% & ', alpaca_good_mt1, '\\% & ', alpaca_good_mt2, '\\% & ', alpaca_good_both,
-              '\\% & ', alpaca_good_mt1_wrttot, '\\% & ', alpaca_good_mt2_wrttot, '\\% & ', alpaca_good_both_wrttot,
-        '\\% \\\\')
+        if has_mg1g2:
+            alpaca_good_mg1g2 = round(100*(n_alpaca_good_mg1g2)/has_truth,1)
+            alpaca_good_mg1g2_wrttot = round(100*(n_alpaca_good_mg1g2)/total,1)
+
+
+            print(max_jet,' & ', has_truth_wrttot , '\\% & ', alpaca_good_mg1g2 , '\\% & ',
+                  alpaca_good_mg1, '\\% & ', alpaca_good_mg2, '\\% & ', alpaca_good_both,
+                  '\\% & ', alpaca_good_mg1g2_wrttot, 
+                  '\\% & ', alpaca_good_mg1_wrttot, '\\% & ', alpaca_good_mg2_wrttot, '\\% & ', alpaca_good_both_wrttot,
+                  '\\% \\\\')
+        else:
+            print(max_jet,' & ', has_truth_wrttot , 
+                  alpaca_good_mg1, '\\% & ', alpaca_good_mg2, '\\% & ', alpaca_good_both,
+                  '\\% & ', alpaca_good_mg1_wrttot, '\\% & ', alpaca_good_mg2_wrttot, '\\% & ', alpaca_good_both_wrttot,
+                  '\\% \\\\')
         print('\\hline')
 
 if by_jet:
